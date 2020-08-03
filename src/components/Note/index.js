ec24a7fe8de2ebe20713a9b9a448b82d486935ba
George Colon <gcolon021@gmail.com>
2020-08-03 17:45:29 -0400

    first commit

---
+++ src/components/Note/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import * as S from './styles';
+
+const index = () => {
+    return (
+        <S.container>
+            Hello
+        </S.container>
+    )
+}
+
+export default index
---
+++ src/components/Note/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const container = styled.div`
+    min-height: 250px;
+    min-width: 250px;
+    width: 250px;
+    background-color: #000;
+    color: #fff;
+    margin: 10px;
+    padding: 2px;
+    box-sizing: border-box;
+    overflow: auto;
+    resize: both;
+`;
